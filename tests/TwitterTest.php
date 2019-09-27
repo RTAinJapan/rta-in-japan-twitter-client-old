11452f66dd41a09c52ce2c48ddd1a885915ec4be
cma-desktop <yamanaka-team@hotmail.co.jp>
2019-09-28 03:53:54 +0900

    Initial commit for testing

---
+++ tests/TwitterTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class TwitterTest extends TestCase
+{
+    public function testGetUserTimeline():void
+    {
+        $this->markTestIncomplete('WIP.');
+    }
+
+    public function testGetMentionsTimeline():void
+    {
+        $this->markTestIncomplete('WIP.');
+    }
+
+    public function testDeleteTweet():void
+    {
+        $this->markTestIncomplete('WIP.');
+    }
+
+    public function testPostTweet():void
+    {
+        $this->markTestIncomplete('WIP.');
+    }
+}
